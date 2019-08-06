3858253b39c4cdf777245cc8bf7fb8ac115179b5
spinetree <steve@spinetree.org>
2019-08-06 15:03:05 -0700

    added eslintrc and greeting function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
-"use strict";
+'use strict';
 
-console.log('loading');
+var greet = function() {
+  var name = prompt('Welcome! What\'s your name?');
+  if (name) {window.alert('Welcome, ' + name.toUpperCase());}
+  else { window.alert('Wait, I didn\'t catch that. Let\'s try again.');
+    greet();}
+};
+  // TODO: change this to change the guest html text and is this recursive-loop problematic?
+
+window.onload = greet();
