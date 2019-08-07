f6798337c951fd29e3e34c72d15dfdc1560133e4
spinetree <steve@spinetree.org>
2019-08-07 15:28:53 -0700

    got number guessing working ffs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
 var greet = function() {
@@ -6,37 +7,68 @@ var greet = function() {
   else { window.alert('Wait, I didn\'t catch that. Let\'s try again.');
     greet();}
   console.log('greet ran');
-
 };
 
-window.onload = greet();
+//FIXME:uncomment for debug
+// window.onload = greet();
 
 var guess = function() {
 
   console.log('guess fired');
 
-  var questions = ['I used to be a mime','There\'s a piece of plastic lodged in my knuckle from when I was a kid and crashed a big wheel','I grew up calling all soda \'pop\'','I can eat fifty eggs','My brother runs a vineyard in the finger lakes.' ];
-  var answers = ['T','F','T','F','F'];
+  //3 arrays - tf questions - number questions - multi choice questions
+  //multidimensional arrays are ugly, but maybe that's still the right tool for the job
+  //start by writing the functions to do the  high low and multi-choice bits first
 
-  window.confirm('I\'m going to ask you some true/false questions about me. See if you can guess the answers.');
+  var questions = ['I used to be a mime','There\'s a piece of plastic lodged in my knuckle from when I was a kid and crashed a big wheel','I grew up calling all soda \'pop\'','I can eat fifty eggs','My brother runs a vineyard in the finger lakes.' ];
+  var tfAnswers = ['T','F','T','F','F'];
+  var numAnswers = 37;
+  var countryAnswers = ['ireland','england','germany','france','switzerland','costa rica','panama','austraila','peru','mexico','canada'];
+
+  window.confirm('I\'m going to ask you some questions about me. See if you can guess the answers.');
   console.log('guess finished');
 
   var score = 0;
 
   //loop through questions, ask each, scrub user input and compare the value there to the answers array.
-  for (var i=0; i < questions.length; i++) {
+
+  //FIXME: set this i back to 4 so the questions run after debugging
+  for (var i=0; i < 0; i++) {
     var answer = window.prompt('True or false: ' +questions[i]);
     var cleanAnswer = answer.charAt(0).toUpperCase();
     console.log(cleanAnswer);
-    if (cleanAnswer === answers[i]) {
+    if (cleanAnswer === tfAnswers[i]) {
       window.confirm('Correct!');
       score++;}
     else {
       window.confirm('NOPE.');
     }
   }
-  window.confirm('Not bad! You got '+ score + ' out of 5 correct.');
+
+  var guesses = 6;
+  while (guesses > 0) {
+    answer = parseInt(window.prompt('Can you guess how freaking ancient I am? You have ' + guesses +' guesses left'));
+    if (answer === 37) {
+      window.confirm('YUP. WTH RIGHT?');
+      guesses = 0;
+      score ++;
+    } else if (answer < 37) {
+      window.confirm('OLDER. Try again. You have ' + guesses +' guesses left');
+      guesses --;
+      console.log(answer);
+    } else if (answer > 37) {
+      window.confirm('Okay not *that* old ffs. Try again. You have ' + guesses +' guesses left');
+      guesses --;
+      console.log(answer);
+    } else {
+      window.confirm('I\'m not sure that\'s even a number bruh.');
+      guesses = 0;}
+    console.log(answer);
+  }
 };
+
+//FIXME: uncomment this to run again
+//window.confirm('Not bad! You got '+ score + ' out of 7 correct.');
 
 document.getElementById('guess-button').onclick = function() {
   guess();
